3db81a18f776e281077299cb128b61dfa12b7801
Ivan Ivanenko <ivan.ivanenko1996@gmail.com>
2024-02-01 11:10:10 +0200

    As an app developer I want to get a list of all libraries to show in my app [Delivers #186961086]

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const port = 8080;
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./docs/swagger.json');
 
+app.get('/libraries', (req, res) => {
+    res.send(['Mustamäe','Lasnamäe','Kopli','Kristiine']);
+});
+
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.listen(port, () => {
     console.log(`API up at: http://localhost:${port}`);
